dfb43eda07990c188c741fff236a712b06dba8a4
Bryan Pintado <bryanp.0311@gmail.com>
2025-04-07 17:35:14 -0400

    java fxml imported and test login screen implemented

---
+++ src/photos22/App.java
@@ -0,0 +1,23 @@
+package photos22;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load the login screen FXML file from the view folder
+        Parent root = FXMLLoader.load(getClass().getResource("view/login.fxml"));
+        primaryStage.setTitle("Photo Application Login");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}   
