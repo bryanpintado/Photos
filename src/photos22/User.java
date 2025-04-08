bb7fdc9690da085baba71962326ff26dadf24148
Bryan Pintado <bryanp.0311@gmail.com>
2025-04-08 15:05:08 -0400

    Implement User and UserManager base structure
    - Added constructor, toString(), and getUsername() in User.java
    - Added user list and deleteUser() method in UserManager.java

--- src/photos22/User.java
+++ src/photos22/User.java
@@ -1,5 +1,18 @@
 package photos22;
 
 public class User {
+    private String username;
 
+    public User(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
--- src/photos22/UserManager.java
+++ src/photos22/UserManager.java
@@ -1,5 +1,21 @@
 package photos22;
 
+import java.util.ArrayList;
+
 public class UserManager {
+    ArrayList<User> users;
+
+    public UserManager() {
+        users = new ArrayList<>();
+    }
+
+    public void deleteUser(String username) {
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getUsername().equals(username)) {
+                users.remove(i);
+                break;
+            }
+        }
+    }
 
 }
