d5862a1895cc44285fc9f290653b69832ccef7e0
Bryan Pintado <bryanp.0311@gmail.com>
2025-04-08 12:09:48 -0400

    Initialized AdminController.java and refactor admin.fxml - linked userListView from fxml to controller - Prepared controller for user management - User class needs to be created

---
+++ src/photos22/controller/AdminController.java
@@ -0,0 +1,16 @@
+package photos22.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextInputDialog;
+import javafx.event.ActionEvent;
+
+public class AdminController {
+    
+@FXML ListView<Users> userListView;
+
+
+
+
+
+}
