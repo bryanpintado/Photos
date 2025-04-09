8dc5a6863ebdb3a9c090c40d466b3c74e249b75c
Bryan Pintado <bryanp.0311@gmail.com>
2025-04-09 13:12:28 -0400

    Added utility class AlertUtil for method accessibility - Allows conistent showAlert usage across controllers - Enables reuse without duplicating alert code

---
+++ src/photos22/AlertUtil.java
@@ -0,0 +1,11 @@
+package photos22;
+public class AlertUtil {
+    
+    public static void showAlert(String message) {
+        javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
