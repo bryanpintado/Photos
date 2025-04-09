7528091412aa4a7e7b0d544b55e576dbe8d47b40
Bryan Pintado <bryanp.0311@gmail.com>
2025-04-08 23:13:52 -0400

    Implement working handleCreateUser - Prompts for username and adds new user to list - TODO: Add duplicate user check

--- src/photos22/controller/AdminController.java
+++ src/photos22/controller/AdminController.java
@@ -4,9 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextInputDialog;
-
 import java.util.Optional;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import photos22.User; // Import the User class
 import photos22.UserManager;
@@ -15,15 +16,24 @@ public class AdminController {
 
     @FXML
     private ListView<User> userListView;
+    private ObservableList<User> users = FXCollections.observableArrayList();
+
+    @FXML
+    private void initialize() {
+        userListView.setItems(users);
+    }
 
     @FXML
     private void handleCreateUser(ActionEvent event) {
         TextInputDialog dialog = new TextInputDialog();
         dialog.setHeaderText("Enter new username");
         String username = dialog.showAndWait().orElse(null);
-        if (username.trim().isEmpty() || username == null) {
+        if (username == null || username.trim().isEmpty()) {
             showAlert("Error: Username cannot be empty");
+            return;
         }
+        User newUser = new User(username.trim());
+        users.add(newUser);
     }
 
     @FXML
