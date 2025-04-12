945d2ef39ed8906db0d8c23756beb62f24dbf1db
Bryan Pintado <bryanp.0311@gmail.com>
2025-04-12 11:54:33 -0400

    classes added and model folder created

---
+++ src/photos22/model/Photo.java
@@ -0,0 +1,74 @@
+package photos22.model;
+
+import java.io.File;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Photo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private File file;
+    private String caption;
+    private LocalDateTime dateTime;
+    private List<Tag> tags;
+
+    public Photo(File file) {
+        this.file = file;
+        this.caption = "";
+        this.tags = new ArrayList<>();
+        this.dateTime = extractModifiedDate(file);
+    }
+
+    private LocalDateTime extractModifiedDate(File file) {
+        try {
+            BasicFileAttributes attrs = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+            return LocalDateTime.ofInstant(attrs.lastModifiedTime().toInstant(), ZoneId.systemDefault());
+        } catch (Exception e) {
+            return LocalDateTime.now();
+        }
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Photo)) return false;
+        Photo other = (Photo) o;
+        return file.getAbsolutePath().equalsIgnoreCase(other.file.getAbsolutePath());
+    }
+
+    @Override
+    public int hashCode() {
+        return file.getAbsolutePath().toLowerCase().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return caption.isEmpty() ? file.getName() : caption;
+    }
+}
---
+++ src/photos22/model/Tag.java
@@ -0,0 +1,5 @@
+package photos22.model;
+
+public class Tag {
+    
+}
