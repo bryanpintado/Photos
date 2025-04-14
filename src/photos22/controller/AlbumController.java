536fc3c60f4f8febd9eb6e23f546e8411315903a
Bryan Pintado <bryanp.0311@gmail.com>
2025-04-14 19:18:49 -0400

    Set up album photo view and add photo support - Created album_view.fxml layout with photo list and control buttons - Added AlbumController.java to manage photo-related actions - Implemented handleAddPhoto() with duplicate check and persistence - Stubbed out handleRemovePhoto, handleViewPhoto, and handleBack - Verified scene loads and FXML bindings function correctly

--- src/photos22/controller/AlbumController.java
+++ src/photos22/controller/AlbumController.java
@@ -1,11 +1,16 @@
 package photos22.controller;
 
+import java.io.File;
+
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
 import photos22.model.Album;
 import photos22.model.Photo;
+import photos22.model.UserManager;
+import photos22.util.AlertUtil;
 
 public class AlbumController {
     private Album album;
@@ -22,5 +27,44 @@ public class AlbumController {
         photoListView.setItems(FXCollections.observableArrayList(album.getPhotos()));
     }
 
-    // TODO: handleRemovePhoto, handleViewPhoto, handleBack
+    @FXML
+    private void handleAddPhoto() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Photo");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.jpeg", "*.png", "*.bmp", "*.gif"));
+
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile == null) {
+            return;
+        }
+
+        String filePath = selectedFile.getAbsolutePath();
+        for (Photo p : album.getPhotos()) {
+            if (p.getFile().getAbsolutePath().equals(filePath)) {
+                AlertUtil.showAlert("This photo is already in the album.");
+                return;
+            }
+        }
+
+        Photo photo = new Photo(selectedFile);
+        album.getPhotos().add(photo);
+        UserManager.getInstance().saveUsers();
+        photoListView.setItems(FXCollections.observableArrayList(album.getPhotos()));
+    }
+
+    @FXML
+    private void handleRemovePhoto() {
+        // TODO: Implement remove selected photo
+    }
+
+    @FXML
+    private void handleViewPhoto() {
+        // TODO: Implement view selected photo in detail
+    }
+
+    @FXML
+    private void handleBack() {
+        // TODO: Implement return to user album list
+    }
 }
